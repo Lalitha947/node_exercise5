d5d159ede64a42f910df1a364aaad58fabb2edff
Lalitha947 <lalithancana415@gmail.com>
2025-07-21 14:47:38 +0200

    edited

--- index.js
+++ index.js
@@ -9,18 +9,31 @@ const pool = mysql.createPool({
 });
 
 const app = express();
+app.listen(7786, () => {
+    console.log('http://localhost:7786');
+})
 
 const products = async ()=>{
  let [row] = await pool.query(`SELECT * FROM products;`,)
  return row
 }
-console.log( await products());
+app.get('/products',async(req,res)=>{
+    res.json({
+        data: await products()
+    })
+})
+
 
 const fetchSinglePoducts = async (code) => {
     let [[row]] = await pool.query(`SELECT * FROM shopleft.products where product_code=?;`, [code]);
     return row;
 }
-console.log( await fetchSinglePoducts('bato 1'))
+app.get('/products',async(req,res)=>{
+    res.json({
+        data: await fetchSinglePoducts('bato 1')
+    })
+})
+
 
 
 const addProducts = async (product_code,product_name,product_price,product_quantity) => {
@@ -31,14 +44,24 @@ const addProducts = async (product_code,product_name,product_price,product_quant
     let [rows] = await pool.query(`SELECT * FROM products;`);
     return rows;
 }
-console.log(await addProducts('tota 1','Bar One','9.00',10));
+app.post('/products',async(req,res)=>{
+    res.json({
+        data: await addProducts('tota 4','Bar One','9.00',10)
+    })
+})
+
 
 
 
 const deleteProducts = async (product_code) => {
     await pool.query(`DELETE FROM products WHERE product_code = ?`, [product_code]);
 };
-await deleteProducts('rant 1');
+app.delete('/products',async(req,res)=>{
+    res.json({
+        data: await deleteProducts('rant 1')
+    })
+})
+
 
 const updateProducts = async (product_code, product_name, product_price, product_quantity) => {
     await pool.query(
@@ -48,19 +71,34 @@ const updateProducts = async (product_code, product_name, product_price, product
     let [row] = await pool.query(`SELECT * FROM products WHERE product_code = ?`, [product_code]);
     return row;
 };
-console.log(await updateProducts('hand 1', 'Sunlight', '20.00', 6));
+app.patch('/products',async(req,res)=>{
+    res.json({
+        data: await updateProducts('hand 1', 'Sunlight', '20.00', 6)
+    })
+})
+
 
 const users = async ()=>{
  let [row] = await pool.query(`SELECT * FROM users;`,)
  return row
 }
-console.log( await users());
+app.get('/users',async(req,res)=>{
+    res.json({
+        data: await users()
+    })
+})
+
 
 const fetchSingleUser = async (id) => {
     let [[row]] = await pool.query(`SELECT * FROM shopleft.users where id=?;`, [id]);
     return row;
 }
-console.log( await fetchSingleUser(2))
+app.get('/users',async(req,res)=>{
+    res.json({
+        data: await fetchSingleUser('2')
+    })
+})
+
 
 const addUsers = async (id,email,first_name,last_name,password) => {
     await pool.query(
@@ -70,10 +108,20 @@ const addUsers = async (id,email,first_name,last_name,password) => {
     let [rows] = await pool.query(`SELECT * FROM users;`);
     return rows;
 }
-console.log(await addUsers(3,'raeesa@gmail.com','raeesa','samaai','coolgurl'));
+app.post('/users',async(req,res)=>{
+    res.json({
+        data: await addUsers(4,'ZAYAAN@gmail.com','zayaan','salie','hotgurl')
+    })
+})
+
 
 const deleteUsers = async (id) => {
-    await pool.query(`DELETE FROM users WHERE id = ?`, [id]);
+    await pool.query(`DELETE FROM users WHERE id = 4`, [id]);
 };
-await deleteUsers(1);
+app.delete('/users',async(req,res)=>{
+    res.json({
+        data: await deleteUsers(4)
+    })
+})
+
 
